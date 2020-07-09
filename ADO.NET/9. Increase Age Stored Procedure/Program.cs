ca282086ed213c896a9672813ca473c514c7e8a2
DraPpeRr <drapperr@users.noreply.github.com>
2020-07-09 20:57:39 +0300

    update

---
+++ ADO.NET/9. Increase Age Stored Procedure/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace _9._Increase_Age_Stored_Procedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = int.Parse(Console.ReadLine());
+
+            using SqlConnection connection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true");
+            connection.Open();
+
+            using SqlCommand increaseAgeCmd = new SqlCommand("usp_GetOlder", connection);
+
+            increaseAgeCmd.CommandType = CommandType.StoredProcedure;
+            increaseAgeCmd.Parameters.AddWithValue("@Id", id);
+
+            increaseAgeCmd.ExecuteNonQuery();
+
+            using SqlCommand getMinionInfoCmd = new SqlCommand($"SELECT Name, Age FROM Minions WHERE Id = {id}", connection);
+
+            using SqlDataReader reader = getMinionInfoCmd.ExecuteReader();
+
+            reader.Read();
+
+            Console.WriteLine($"{reader["Name"]} – {reader["Age"]} years old");
+        }
+    }
+}

cd1c131b1c3af5cc5a0de9cf86def9b7355faa79
DraPpeRr <drapperr@users.noreply.github.com>
2020-07-14 23:07:43 +0300

    update

---
+++ Entity Relations/P01_StudentSystem/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using System;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected StudentSystemContext()
+        {
+        }
+
+        public DbSet<Course> MyProperty { get; set; }
+    }
+}

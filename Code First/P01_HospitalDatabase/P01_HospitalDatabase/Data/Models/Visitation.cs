5f50eb9595d5e9105c14eab3052f8f4312fd27ee
DraPpeRr <drapperr@users.noreply.github.com>
2020-07-21 07:50:14 +0300

    update

---
+++ Code First/P01_HospitalDatabase/P01_HospitalDatabase/Data/Models/Visitation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Visitation
+    {
+        [Key]
+        public int VisitationId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [MaxLength(250)]
+        public string Comments { get; set; }
+
+        public int PatientId { get; set; }
+
+        [ForeignKey(nameof(PatientId))]
+        public Patient Patient { get; set; }
+
+        public int? DoctorId { get; set; }
+
+        [ForeignKey(nameof(DoctorId))]
+        public Doctor Doctor { get; set; }
+    }
+}

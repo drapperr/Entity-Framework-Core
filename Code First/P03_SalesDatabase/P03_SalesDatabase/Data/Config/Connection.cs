bf8a042bffa5ae904a798d29a45a55ba79b289f0
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-15 15:23:53 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/Data/Config/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Config
+{
+    public static class Connection
+    {
+        public static string ConnectionString = @"Server=.;Database=SalesDb;Integrated Security=True";
+    }
+}

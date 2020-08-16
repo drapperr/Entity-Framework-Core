cc8b2fdc93077de58019c1de8620ccf491d34b4d
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-16 20:33:12 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/Data/Seeding/StoreSeeder.cs
@@ -0,0 +1,35 @@
+﻿using P03_SalesDatabase.Data.Models;
+using P03_SalesDatabase.Data.Seeding.Contracts;
+using P03_SalesDatabase.IOMangement.Contracts;
+
+namespace P03_SalesDatabase.Data.Seeding
+{
+    public class StoreSeeder : ISeeder
+    {
+        private SalesContext dbContext;
+        private readonly IWriter writer;
+
+        public StoreSeeder(SalesContext context,IWriter  writer)
+        {
+            this.dbContext = context;
+            this.writer = writer;
+        }
+        public void Seed()
+        {
+            Store[] stores = new Store[] 
+            { 
+                new Store() { Name = "PcTech Sofia"},
+                new Store() { Name = "PcTech Plovdiv"},
+                new Store() { Name = "PcTech Varna"},
+                new Store() { Name = "InnovativeTech Sofia"},
+                new Store() { Name = "InnovativeTech Plovdiv"},
+            };
+
+            this.dbContext.Stores.AddRange(stores);
+
+            this.dbContext.SaveChanges();
+
+            this.writer.WriteLine($"{stores.Length} stores were added to the database");
+        }
+    }
+}

abad01eba31bae9bd5cd1c2f3dc942f7716f2cd3
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-16 20:31:57 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Config;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+        }
+
+        public SalesContext(DbContextOptions dbContextOptions) 
+            : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Connection.ConnectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name).IsUnicode();
+
+                entity.Property(e => e.Description).HasDefaultValue("No description");
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Name).IsUnicode();
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.CreditCardNumber).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.Property(e => e.Name).IsUnicode();
+            });
+
+            modelBuilder.Entity<Sale>(entity=> 
+            {
+                entity.Property(e => e.Date).HasDefaultValueSql("GETADE()");
+            });
+        }
+    }
+}

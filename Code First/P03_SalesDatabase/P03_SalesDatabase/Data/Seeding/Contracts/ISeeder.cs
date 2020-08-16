1b873b4638a069097b6536ba436354ece9432ce9
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-16 20:32:21 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/Data/Seeding/Contracts/ISeeder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Seeding.Contracts
+{
+    interface ISeeder
+    {
+        void Seed();
+    }
+}

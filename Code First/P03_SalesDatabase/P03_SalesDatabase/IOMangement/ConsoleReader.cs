b6e9a5e15f1ecee5174366a5dcca5820bd7e0105
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-16 20:33:37 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/IOMangement/ConsoleReader.cs
@@ -0,0 +1,15 @@
+﻿using P03_SalesDatabase.IOMangement.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.IOMangement
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}

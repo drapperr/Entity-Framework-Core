4034e553dd3cc7517972c4904e70df2ed6fce8aa
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-17 21:39:51 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/IOMangement/Contracts/IWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.IOMangement.Contracts
+{
+    public interface IWriter
+    {
+        void Write(string text);
+        void WriteLine(string text);
+    }
+}

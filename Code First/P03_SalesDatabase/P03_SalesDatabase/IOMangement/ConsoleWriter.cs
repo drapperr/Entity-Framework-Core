d5ba83c70380718c6fece6ff898c139c86933b7a
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-17 21:37:52 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/IOMangement/ConsoleWriter.cs
@@ -0,0 +1,20 @@
+﻿using P03_SalesDatabase.IOMangement.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.IOMangement
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void Write(string text)
+        {
+            Console.Write(text);
+        }
+
+        public void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}

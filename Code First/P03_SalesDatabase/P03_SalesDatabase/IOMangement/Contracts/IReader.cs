4ec6b567f6cc1a4980dfa6bf4b47d2aa05d20216
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-17 21:39:29 +0300

    update

---
+++ Code First/P03_SalesDatabase/P03_SalesDatabase/IOMangement/Contracts/IReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.IOMangement.Contracts
+{
+    public interface IReader
+    {
+        string ReadLine();
+    }
+}

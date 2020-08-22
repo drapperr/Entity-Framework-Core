514c73addd916b3817a0444d5208959537265ea0
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-22 07:22:35 +0300

    update

---
+++ Exam/VaporStore/DataProcessor/Dto/Export/ExportGenreDto.cs
@@ -0,0 +1,32 @@
+﻿namespace VaporStore.DataProcessor.Dto.Export
+{
+    public class ExportGenreDto
+    {
+        public int Id { get; set; }
+
+        public string Genre { get; set; }
+
+        public ExportGameDto[] Games { get; set; }
+
+        public int TotalPlayers { get; set; }
+    }
+}
+
+
+
+                    //Id = g.Id,
+                    //Genre = g.Name,
+                    //Games = g.Games
+                    //.Where(ga => ga.Purchases.Count != 0)
+                    //.Select(ga => new
+                    //{
+                    //    Id = ga.Id,
+                    //    Title = ga.Name,
+                    //    Developer = ga.Developer.Name,
+                    //    Tags = ga.GameTags.Select(t => string.Join(", ", t.Tag.Name)),
+                    //    Players = ga.Purchases.Count()
+                    //})
+                    //.ToArray()
+                    //.OrderByDescending(x=>x.Players)
+                    //.ThenBy(x=>x.Id),
+                    //TotalPlayers = g.Games.Sum(ga => ga.Purchases.Count)

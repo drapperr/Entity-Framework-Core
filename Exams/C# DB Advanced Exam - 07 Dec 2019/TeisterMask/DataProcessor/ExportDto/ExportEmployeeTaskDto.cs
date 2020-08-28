9eee43d20115f28638a66ef1042baa59b730e4de
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-28 20:53:14 +0300

    update

---
+++ Exams/C# DB Advanced Exam - 07 Dec 2019/TeisterMask/DataProcessor/ExportDto/ExportEmployeeTaskDto.cs
@@ -0,0 +1,17 @@
+﻿using TeisterMask.Data.Models.Enums;
+
+namespace TeisterMask.DataProcessor.ExportDto
+{
+    public class ExportEmployeeTaskDto
+    {
+        public string TaskName { get; set; }
+
+        public string OpenDate { get; set; }
+
+        public string DueDate { get; set; }
+
+        public string LabelType { get; set; }
+
+        public string ExecutionType { get; set; }
+    }
+}

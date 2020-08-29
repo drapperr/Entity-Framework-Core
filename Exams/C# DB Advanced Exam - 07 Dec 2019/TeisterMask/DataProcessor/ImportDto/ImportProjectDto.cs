a41629ffc8580d115f19e63be857e856ef5b03e2
DraPpeRr <drapperr@users.noreply.github.com>
2020-08-29 22:55:20 +0300

    update

---
+++ Exams/C# DB Advanced Exam - 07 Dec 2019/TeisterMask/DataProcessor/ImportDto/ImportProjectDto.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Project")]
+    public class ImportProjectDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [XmlElement("OpenDate")]
+        [Required]
+        public string OpenDate { get; set; }
+
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; }
+
+        [XmlArray("Tasks")]
+        public ImportProjectTaskDto[] Tasks { get; set; }
+    }
+}
+
+//<Project>
+//    <Name>S</Name>
+//    <OpenDate>25/01/2018</OpenDate>
+//    <DueDate>16/08/2019</DueDate>
+//    <Tasks>
+//      <Task>
+//        <Name>Australian</Name>
+//        <OpenDate>19/08/2018</OpenDate>
+//        <DueDate>13/07/2019</DueDate>
+//        <ExecutionType>2</ExecutionType>
+//        <LabelType>0</LabelType>
+//      </Task>

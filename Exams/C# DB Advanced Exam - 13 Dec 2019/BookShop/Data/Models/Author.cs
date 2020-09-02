87b19c5f534b201d484350de769945b77551b695
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-02 20:44:34 +0300

    update

---
+++ Exams/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/Models/Author.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookShop.Data.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            this.AuthorsBooks = new List<AuthorBook>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(12)]
+        public string Phone { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}

b7a182aa7bdc1d5e82ab0f91d179dd329938f1a5
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-02 20:45:40 +0300

    update

---
+++ Exams/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using BookShop.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShop.Data.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+            this.AuthorsBooks = new List<AuthorBook>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Pages { get; set; }
+
+        [Required]
+        public DateTime PublishedOn { get; set; }
+
+        [ForeignKey("BookId")]
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}

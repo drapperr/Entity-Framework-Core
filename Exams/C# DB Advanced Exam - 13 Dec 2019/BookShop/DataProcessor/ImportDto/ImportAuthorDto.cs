1e5721f3f1c88d5b4e2048a716c9feadb4831aba
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-03 19:58:17 +0300

    update

---
+++ Exams/C# DB Advanced Exam - 13 Dec 2019/BookShop/DataProcessor/ImportDto/ImportAuthorDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class ImportAuthorDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public List<ImportAuthorBookDto> Books { get; set; }
+    }
+}
+
+
+//    "FirstName": "K",
+//    "LastName": "Tribbeck",
+//    "Phone": "808-944-5051",
+//    "Email": "btribbeck0@last.fm",
+//    "Books": [
+//      {
+//        "Id": 79
+//      },
+//      {
+//        "Id": 40
+//      }

1236d80db7abfdd859a68f1d123a0465ffa6fcb2
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-03 19:57:30 +0300

    update

---
+++ Exams/C# DB Advanced Exam - 13 Dec 2019/BookShop/DataProcessor/ImportDto/ImportAuthorBookDto.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class ImportAuthorBookDto
+    {
+        public int? Id { get; set; }
+    }
+}

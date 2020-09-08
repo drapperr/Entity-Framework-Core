44c2595cf7e640d1b8312c6d580fe4a9a9163706
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-08 20:27:13 +0300

    update

---
+++ Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ExportDtos/ExportAlbumDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicHub.DataProcessor.ExportDtos
+{
+    public class ExportAlbumDto
+    {
+        public string AlbumName { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string ProducerName { get; set; }
+
+        public ExportAlbumSongDto[] Songs { get; set; }
+
+        public string AlbumPrice { get; set; }
+    }
+}
+
+
+//"AlbumName": "Devil's advocate",
+//    "ReleaseDate": "07/21/2018",
+//    "ProducerName": "Evgeni Dimitrov",
+//    "Songs": [
+//      {
+//        "SongName": "Numb",
+//        "Price": "13.99",
+//        "Writer": "Kara-lynn Sharpous"
+//      },

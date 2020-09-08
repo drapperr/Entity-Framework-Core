4c7505d07c697d76f6926639140222031a7ce5fa
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-08 20:27:30 +0300

    update

---
+++ Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ExportDtos/ExportAlbumSongDto.cs
@@ -0,0 +1,19 @@
+﻿namespace MusicHub.DataProcessor.ExportDtos
+{
+    public class ExportAlbumSongDto
+    {
+        public string SongName { get; set; }
+
+        public string Price { get; set; }
+
+        public string Writer { get; set; }
+
+    }
+}
+
+//{
+//        "SongName": "Numb",
+//        "Price": "13.99",
+//        "Writer": "Kara-lynn Sharpous"
+//      },
+

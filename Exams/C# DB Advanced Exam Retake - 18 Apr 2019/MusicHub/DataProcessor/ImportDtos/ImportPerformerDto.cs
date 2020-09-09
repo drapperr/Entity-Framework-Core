1907c4c8258b6e3d5026741db5de987ef9a62ea8
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-09 19:33:40 +0300

    update

---
+++ Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ImportDtos/ImportPerformerDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    [XmlType("Performer")]
+    public class ImportPerformerDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        [XmlElement("FirstName")]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        [XmlElement("LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(18, 70)]
+        [XmlElement("Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        [XmlElement("NetWorth")]
+        public decimal NetWorth { get; set; }
+
+        [XmlArray("PerformersSongs")]
+        public ImportPerformerSongDto[] PerformersSongs { get; set; }
+    }
+}

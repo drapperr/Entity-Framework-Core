a52d3f5c612cd083dfae8390178c111f58f626b1
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-09 19:34:03 +0300

    update

---
+++ Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ImportDtos/ImportProducerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    public class ImportProducerDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [RegularExpression("^[A-Z][a-z][a-z]+ [A-Z][a-z][a-z]+$")]
+        public string Pseudonym { get; set; }
+
+        [RegularExpression(@"^\+359 \d{3} \d{3} \d{3}$")]
+        public string PhoneNumber { get; set; }
+
+        public ImportAlbumDto[] Albums { get; set; }
+    }
+}

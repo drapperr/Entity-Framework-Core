55f080e8042b7f84f319fb821d3164bd5cb3fbc3
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-10 19:38:43 +0300

    update

---
+++ Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ImportDtos/ImportSongDto.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Serialization;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    [XmlType("Song")]
+    public class ImportSongDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [XmlElement("Duration")]
+        [Required]
+        public string Duration { get; set; }
+
+        [XmlElement("CreatedOn")]
+        [Required]
+        public string CreatedOn { get; set; }
+
+        [XmlElement("Genre")]
+        [Required]
+        public string Genre { get; set; }
+
+        [XmlElement("AlbumId")]
+        [Required]
+        public int? AlbumId { get; set; }
+
+        [XmlElement("WriterId")]
+        [Required]
+        public int WriterId { get; set; }
+
+        [XmlElement("Price")]
+        [Required]
+        [Range(typeof(decimal),"0", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+    }
+}

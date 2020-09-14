41e1040b111bd2d0795b181632b12ba60e201845
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-14 22:04:29 +0300

    update

---
+++ JSON/CarDealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO.CarDTO;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<CarsImportDTO, Car>();
+
+        }
+    }
+}

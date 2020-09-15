47fdd387275b70bed16df01117cfc6872a7736da
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-15 17:43:31 +0300

    update

---
+++ JSON/CarDealer/CarDealer/DTO/CarDTO/CarsImportDTO.cs
@@ -0,0 +1,12 @@
+﻿using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO.CarDTO
+{
+    public class CarsImportDTO : Car
+    {
+        public int[] partsId { get; set; }
+    }
+}

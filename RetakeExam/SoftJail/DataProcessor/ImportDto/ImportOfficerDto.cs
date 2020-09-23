766f611d950ffd0408fb7d6aeca7fc744ca74fc0
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-23 22:11:53 +0300

    update

---
+++ RetakeExam/SoftJail/DataProcessor/ImportDto/ImportOfficerDto.cs
@@ -0,0 +1,52 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class ImportOfficerDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [XmlElement("Money")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Money { get; set; }
+
+        [XmlElement("Position")]
+        [Required]
+        public string Position { get; set; }
+
+        [XmlElement("Weapon")]
+        [Required]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public PrisonerDto[] Prisoners { get; set; }
+    }
+
+    [XmlType("Prisoner")]
+    public class PrisonerDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+    }
+}
+
+//  <Officer>
+//    <Name>Minerva Kitchingman</Name>
+//    <Money>2582</Money>
+//    <Position>Invalid</Position>
+//    <Weapon>ChainRifle</Weapon>
+//    <DepartmentId>2</DepartmentId>
+//    <Prisoners>
+//      <Prisoner id = "15" />
+//    </ Prisoners >
+//  </ Officer >

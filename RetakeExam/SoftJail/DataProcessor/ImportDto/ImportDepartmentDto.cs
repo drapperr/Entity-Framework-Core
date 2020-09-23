d01dada8d8d53437f837160663181f27fb15232f
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-23 22:11:23 +0300

    update

---
+++ RetakeExam/SoftJail/DataProcessor/ImportDto/ImportDepartmentDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class ImportDepartmentDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        public ImportCellDto[] Cells { get; set; }
+    }
+
+    public class ImportCellDto
+    {
+        [Required]
+        [Range(1, 1000)]
+        public int CellNumber { get; set; }
+
+        [Required]
+        public bool HasWindow { get; set; }
+    }
+}
+
+
+//"Name": "",
+//    "Cells": [
+//      {
+//        "CellNumber": 101,
+//        "HasWindow": true
+//      },
+//      {
+//        "CellNumber": 102,
+//        "HasWindow": false
+//      }

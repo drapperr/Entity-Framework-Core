5623e6a8161ed5a6608777200f4ca973bfeb838e
DraPpeRr <drapperr@users.noreply.github.com>
2020-09-30 22:49:54 +0300

    update

---
+++ XML/CarDealer/CarDealer/Dto/Export/ExportCustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.Dto.Export
+{
+    [XmlType("customer")]
+    public class ExportCustomerDto
+    {
+        [XmlAttribute("full-name")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("bought-cars")]
+        public int BoughtCars { get; set; }
+
+        [XmlAttribute("spent-money")]
+        public decimal SpentMoney { get; set; }
+    }
+}
+
+//<customer full-name="Hai Everton" bought-cars="1" spent-money="2544.67" />

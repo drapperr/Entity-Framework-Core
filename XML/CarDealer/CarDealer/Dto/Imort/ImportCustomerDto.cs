3e8a8794dee833d317a9e56560a05978ce6b5b72
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-01 18:30:35 +0300

    update

---
+++ XML/CarDealer/CarDealer/Dto/Imort/ImportCustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CarDealer.Dto.Imort
+{
+    [XmlType("Customer")]
+    public class ImportCustomerDto
+    {
+        [XmlElement("name")]
+        public string name { get; set; }
+
+        [XmlElement("birthDate")]
+        public DateTime BirthDate { get; set; }
+
+        [XmlElement("isYoungDriver")]
+        public bool IsYanoungDriver { get; set; }
+    }
+}
+
+
+//<Customer>
+//        <name>Emmitt Benally</name>
+//        <birthDate>1993-11-20T00:00:00</birthDate>
+//        <isYoungDriver>true</isYoungDriver>
+//    </Customer>

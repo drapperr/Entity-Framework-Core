196ec9f251a792aefc15a9d583488315ef63d744
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-01 18:29:56 +0300

    update

---
+++ XML/CarDealer/CarDealer/Dto/Export/ExportSaleDto.cs
@@ -0,0 +1,27 @@
+﻿using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.Dto.Export
+{
+    [XmlType("sale")]
+    public class ExportSaleDto
+    {
+        [XmlElement("car")]
+        public ExportCarAttDto Car { get; set; }
+
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+
+        [XmlElement("customer-name")]
+        public string CustomerName { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("price-with-discount")]
+        public decimal PriceWithDiscount { get; set; }
+    }
+}

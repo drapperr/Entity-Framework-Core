b91a71b839afb1d69830effcfac13fcffa01294e
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-02 18:05:11 +0300

    update

---
+++ XML/CarDealer/CarDealer/Dto/Imort/ImportSalesDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.Dto.Imort
+{
+    [XmlType("Sale")]
+    public class ImportSalesDto
+    {
+        [XmlElement("carId")]
+        public int CarId { get; set; }
+
+        [XmlElement("customerId")]
+        public int CustomerId { get; set; }
+
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+    }
+}
+
+//<Sale>
+//        <carId>342</carId>
+//        <customerId>29</customerId>
+//        <discount>0</discount>
+//    </Sale>

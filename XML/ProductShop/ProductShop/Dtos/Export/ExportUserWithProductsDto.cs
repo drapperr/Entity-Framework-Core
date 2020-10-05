99ca98d6018499f433147d0aa25919c29efa820d
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-05 18:31:15 +0300

    update

---
+++ XML/ProductShop/ProductShop/Dtos/Export/ExportUserWithProductsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("Users")]
+    public class ExportUserWithProducts
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("users")]
+        public ExportUser[] Users { get; set; }
+    }
+}

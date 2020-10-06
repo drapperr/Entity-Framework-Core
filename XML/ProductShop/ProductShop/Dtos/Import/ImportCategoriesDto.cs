2bc03362927f779610bf31ec6d0a4ef1efb330cf
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-06 12:45:19 +0300

    update

---
+++ XML/ProductShop/ProductShop/Dtos/Import/ImportCategoriesDto.cs
@@ -0,0 +1,12 @@
+﻿using ProductShop.Models;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Import
+{
+    [XmlType(nameof(Category))]
+    public class ImportCategoriesDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+}

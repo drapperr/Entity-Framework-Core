da58e80582bd8933771b4e0b188c51033cd9893a
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-07 20:26:25 +0300

    update

---
+++ XML/ProductShop/ProductShop/Dtos/Import/ImportUserDto.cs
@@ -0,0 +1,24 @@
+﻿using ProductShop.Models;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Import
+{
+    [XmlType(nameof(User))]
+    public class ImportUserDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public int Age { get; set; }
+    }
+}
+
+ //<User>
+ //       <firstName>Chrissy</firstName>
+ //       <lastName>Falconbridge</lastName>
+ //       <age>50</age>
+ //   </User>
